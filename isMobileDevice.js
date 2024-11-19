7cd9599c64ec5f6ae8cfd22bfd9018375e626335
PigPlayzMC <charliehbird@gmail.com>
2024-11-19 22:01:04 +0000

    Dynamic content maxwidth scaling. Not working

--- isMobileDevice.js
+++ isMobileDevice.js
@@ -4,6 +4,10 @@ function isMobileDevice() {
 }
 if (isMobileDevice()) {
 	console.log("Detected device is a mobile.");
+	const element = document.querySelector('.content');
+	element.style.maxWidth = '95vw';
 } else {
 	console.log("Detected device is not a mobile.");
+	const element = document.querySelector('.content');
+	element.style.maxWidth = '75vw';
 }
