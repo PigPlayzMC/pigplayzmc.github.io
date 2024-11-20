4ea72b7930a38ed9d185cea61f04a08d69cadecd
PigPlayzMC <charliehbird@gmail.com>
2024-11-20 10:44:17 +0000

    Formatted header logo

--- isMobileDevice.js
+++ isMobileDevice.js
@@ -6,11 +6,19 @@ if (isMobileDevice()) {
 	console.log("Mobile formatting in effect.");
 
 	/* Main text formatting */
+	// Less boundary, more text on each line
 	const element = document.querySelector('.content');
 	element.style.maxWidth = '95vw';
 	element.style.marginLeft = '0';
 	element.style.paddingLeft = '5vw';
 	element.style.marginTop = '17vh'; 
+
+	/* Header logo formatting */
+	// 15% scale
+	const headerImage = document.querySelector('.header-image');
+	headerImage.style.scale = '15%';
+
+	
 } else {
 	console.log("Standard formatting in effect.");
 
@@ -20,4 +28,8 @@ if (isMobileDevice()) {
 	element.style.marginLeft = '20';
 	element.style.paddingLeft = '25vw';
 	element.style.marginTop = '15vh';
+
+	/* Header logo formatting */
+	const headerImage = document.querySelector('.header-image');
+	headerImage.style.scale = '20%';
 }
