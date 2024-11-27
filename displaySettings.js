58638f9e2635493eaa5e1a82c477a080c960fc56
PigPlayzMC <charliehbird@gmail.com>
2024-11-27 16:32:52 +0000

    Style formatting now uses localStorage for uniformity

--- displaySettings.js
+++ displaySettings.js
@@ -11,17 +11,22 @@ function darkmode(darkmodeOn) {
     }
 }
 
-function fontSize(size) {
+function fontSize(size, preloading) {
     const element = document.querySelector('.content');
     if (size == 0) {
         console.log("Small font size selected."); // 16px
-        element.style.fontSize = "16px"
+        element.style.fontSize = "16px";
     } else if (size == 1) {
         console.log("Standard font size selected."); // 20px
-        element.style.fontSize = "20px"
+        element.style.fontSize = "20px";
     } else {
         console.log("Large font size selected."); // 24px
-        element.style.fontSize = "24px"
+        element.style.fontSize = "24px";
+    }
+    
+    // Only sets local theme if its been changed.
+    if (!preloading) {
+        localStorage.setItem("Size", size)
     }
 }
 
@@ -33,6 +38,15 @@ function spaces(spacing) {
     }
 }
 
+// Initialise with correct formatting
+try {
+    fontSize(localStorage.getItem("Size"), true);
+    }
+catch {
+    console.log("No previous style settings found")
+}
+
+// Button toggle
 document.getElementById('display-toggle').addEventListener('click', function() {
     const menu = document.getElementById('display-menu');
     menu.classList.toggle('hidden');
@@ -40,11 +54,11 @@ document.getElementById('display-toggle').addEventListener('click', function() {
 
 // Listen for all font size button clicks
 document.getElementById('small-font').addEventListener('click', function() {
-    fontSize(0);
+    fontSize(0, false);
 });
 document.getElementById('standard-font').addEventListener('click', function() {
-    fontSize(1);
+    fontSize(1, false);
 });
 document.getElementById('large-font').addEventListener('click', function() {
-    fontSize(2);
+    fontSize(2, false);
 });
