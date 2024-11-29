72ce8c735719d1baae26e5a2db8951a273be3c8a
PigPlayzMC <charliehbird@gmail.com>
2024-11-29 13:18:35 +0000

    Mobile formatting now dynamically reselects every 10 seconds (Mostly just useful for development.

--- isMobileDevice.js
+++ isMobileDevice.js
@@ -2,32 +2,42 @@ function isMobileDevice() {
 	return window
 		.matchMedia("only screen and (max-width: 760px)").matches;
 }
-if (isMobileDevice()) {
-	console.log("Mobile formatting in effect.");
-
-	/* Main text formatting */
-	// Less boundary, more text on each line
-	const element = document.querySelector('.content');
-	element.style.maxWidth = '95vw';
-	element.style.marginLeft = '0';
-	element.style.paddingLeft = '5vw';
-	element.style.marginTop = '17vh'; 
-
-	/* Header logo formatting */
-	// Hide
-	const headerImage = document.querySelector('.header-image');
-	headerImage.classList.toggle('hidden');
-} else {
-	console.log("Standard formatting in effect.");
-
-	/* Main text formatting */
-	const element = document.querySelector('.content');
-	element.style.maxWidth = '75vw';
-	element.style.marginLeft = '20';
-	element.style.paddingLeft = '25vw';
-	element.style.marginTop = '15vh';
-
-	/* Header logo formatting */
-	const headerImage = document.querySelector('.header-image');
-	headerImage.style.scale = '20%';
+function mobileFormatting() {
+	if (isMobileDevice()) {
+		console.log("Mobile formatting in effect.");
+	
+		/* Main text formatting */
+		// Less boundary, more text on each line
+		const element = document.querySelector('.content');
+		element.style.maxWidth = '95vw';
+		element.style.marginLeft = '0';
+		element.style.paddingLeft = '5vw';
+		element.style.marginTop = '17vh'; 
+	
+		/* Header logo formatting */
+		// Hide
+		const headerImage = document.querySelector('.header-image');
+		if (headerImage.classList.contains('hidden')) {
+			// Do nothing as the image is already hidden!
+		} else {
+			headerImage.classList.toggle('hidden');
+		}
+	} else {
+		console.log("Standard formatting in effect.");
+	
+		/* Main text formatting */
+		const element = document.querySelector('.content');
+		element.style.maxWidth = '75vw';
+		element.style.marginLeft = '20';
+		element.style.paddingLeft = '25vw';
+		element.style.marginTop = '15vh';
+	
+		/* Header logo formatting */
+		const headerImage = document.querySelector('.header-image');
+		headerImage.style.scale = '20%';
+	}
 }
+
+mobileFormatting // Runs on page load
+
+setInterval(mobileFormatting(), 10000) // Rechecks if mobile formatting should be active once every 10 seconds.
