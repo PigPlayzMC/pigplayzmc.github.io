639ac006a9bb97ba27a69dbd42bf44fbfb870990
PigPlayzMC <charliehbird@gmail.com>
2024-12-06 16:52:51 +0000

    No longer toggles both or neither. Now neither work ever.

--- closePopUps.js
+++ closePopUps.js
@@ -1,19 +1,27 @@
 const menu = document.getElementById('menu');
 
-// Add a click event listener to the document
-document.addEventListener('click', function (event) {
-    // Check if the clicked element is outside the target element
-    if (!menu.contains(event.target)) {
-        menu.classList.toggle('hidden');
-    }
-});
+if (menu.classList.contains('hidden')) {
+    // Do nothing. Already hidden
+    } else {
+    // Add a click event listener to the document
+    document.addEventListener('click', function (event) {
+        // Check if the clicked element is outside the target element
+        if (!menu.contains(event.target)) {
+            menu.classList.toggle('hidden');
+        }
+    });
+}
 
 const display = document.getElementById('display-menu');
 
-// Add a click event listener to the document
-document.addEventListener('click', function (event) {
-    // Check if the clicked element is outside the target element
-    if (!display.contains(event.target)) {
-        display.classList.toggle('hidden');
-    }
-});
+if (display.classList.contains('hidden')) {
+    // Do nothing. Already hidden
+    } else {
+    // Add a click event listener to the document
+    document.addEventListener('click', function (event) {
+        // Check if the clicked element is outside the target element
+        if (!display.contains(event.target)) {
+            display.classList.toggle('hidden');
+        }
+    });
+}
