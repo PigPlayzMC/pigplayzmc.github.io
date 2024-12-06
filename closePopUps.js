2a94511ec27eb2973297f71cc15d7c11c1419c74
PigPlayzMC <charliehbird@gmail.com>
2024-12-06 16:47:34 +0000

    closePopUps.js ready for troubleshooting

--- closePopUps.js
+++ closePopUps.js
@@ -1,1 +1,19 @@
-const navigationButton = ;
+const menu = document.getElementById('menu');
+
+// Add a click event listener to the document
+document.addEventListener('click', function (event) {
+    // Check if the clicked element is outside the target element
+    if (!menu.contains(event.target)) {
+        menu.classList.toggle('hidden');
+    }
+});
+
+const display = document.getElementById('display-menu');
+
+// Add a click event listener to the document
+document.addEventListener('click', function (event) {
+    // Check if the clicked element is outside the target element
+    if (!display.contains(event.target)) {
+        display.classList.toggle('hidden');
+    }
+});
